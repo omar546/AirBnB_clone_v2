47531f0e7cf3506c54e34ff116c218a96c13490f
omar546 <omer.mkawy.om@gmail.com>
2024-02-19 19:40:29 +0200

    ness

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,11 +48,11 @@ def numbersandtemplates(n):
 def numbersandevenness(n):
     """display a HTML page only if n is an integer"""
     if n % 2 == 0:
-        evenorodd = 'even'
+        evenness = 'even'
     else:
-        evenorodd = 'odd'
+        evenness = 'odd'
     return render_template('6-number_odd_or_even.html', n=n,
-                           evenness=evenorodd)
+                           evenness=evenness)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
